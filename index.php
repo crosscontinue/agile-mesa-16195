912591809642d6e16354e7ebf18c7b7629293cc3
Takahiro Takeuchi <t.takeuchi@tmc-labo.com>
2019-07-22 14:42:25 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="ja-JP">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+	<title></title>
+	<meta name="description" content=""/>
+	<meta property="og:title" content=""/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+
+</head>
+<body>
+<header>
+</header>
+<main>
+<div id="wrapper">
+  <section id="visual" class="visual" data-name="">
+    <video src="movie/sigco_10sec.mp4" playsinline muted autoplay loop></video>
+  </section>
+</div>
+
+</main>
+<footer>
+</footer>
+
+<script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="libs/scripts.js"></script>
+
+</body>
+</html>
